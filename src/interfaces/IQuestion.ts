f3a47ec0b22efa48a214b228ee84855adcd213cd
Dmitry Bryuhanov <bryuhanovdima@yandex.ru>
2021-06-22 23:51:45 +0500

    delete unnecessary files

--- src/interfaces/IQuestion.ts
+++
@@ -1,4 +0,0 @@
-export interface IQuestion {
-    resolve: (value?: any) => void;
-    startTime: number;
-}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,38 @@
-export * from './IQuestion';
-export * from './IQuestionParams';
-export * from './IQuestionManagerParams';
-export * from './IQuestionMessageContext';
+import { Params, MessageContext } from 'vk-io';
+
+import { Answer } from '../structures';
+
+export interface IQuestion {
+    resolve: (value?: any) => void;
+    startTime: number;
+}
+
+export interface IQuestionManagerParams {
+    /**
+     * Ограничение на время ответа. Задаётся в ms
+     */
+    answerTimeLimit?: number;
+}
+
+export interface IQuestionParams extends Params.MessagesSendParams {
+    /**
+     * Айди пользователя, которому нужно задать вопрос
+     * @default context.senderId
+     */
+    targetUserId?: number;
+
+    /**
+     * Ограничение на время ответа. Задаётся в ms
+     */
+    answerTimeLimit?: number;
+
+    [key: string]: any;
+}
+
+export interface IQuestionMessageContext extends MessageContext {
+    /**
+     * @param message Текст вопроса
+     * @param params Параметры отправляемого сообщения
+     */
+    question: (message: string, params?: IQuestionParams) => Promise<Answer>;
+}
