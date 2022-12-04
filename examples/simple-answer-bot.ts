0ca2c480426b9443ce6ed4fb8dbff54633994400
fakemancat <bryuhanovdima@yandex.ru>
2022-12-05 01:26:05 +0500

    refactor(examples)

--- examples/simple-answer-bot.ts
+++ examples/simple-answer-bot.ts
@@ -1,18 +1,20 @@
 import { VK, MessageContext } from 'vk-io';
-import { QuestionManager } from '../';
+import { QuestionManager, QuestionMessageContext } from '../';
 
 const vk = new VK({
-    token: process.env.TOKEN
+    token: process.env.TOKEN!
 });
 
 const questionManager = new QuestionManager();
 
-vk.updates.use<MessageContext>(questionManager.middleware);
+vk.updates.use(questionManager.middleware);
 
-vk.updates.on('message', async (context) => {
+type Context = MessageContext & QuestionMessageContext;
+
+vk.updates.on('message', async (context: Context) => {
     const answer = await context.question('Привет, как твоё имя?');
 
-    while (!answer.text) {
+    if (!answer.text) {
         return context.send('Пожалуйста, ответь текстом');
     }
 
--- examples/simple-math-bot.ts
+++ examples/simple-math-bot.ts
@@ -1,18 +1,20 @@
 import { VK, MessageContext } from 'vk-io';
 import { HearManager } from '@vk-io/hear';
-import { QuestionManager, QuestionMessageContext } from '../';
+import { QuestionManager, QuestionMessageContext, TimeoutError } from '../';
 
 const vk = new VK({
     token: process.env.TOKEN!
 });
 
 const questionManager = new QuestionManager();
-const hearManager = new HearManager<QuestionMessageContext>();
+const hearManager = new HearManager();
 
 vk.updates.use(questionManager.middleware);
 vk.updates.on('message', hearManager.middleware);
 
-hearManager.hear('пример', async (context) => {
+type Context = MessageContext & QuestionMessageContext;
+
+hearManager.hear('пример', async (context: Context) => {
     const a = Math.round(Math.random() * 100);
     const b = Math.round(Math.random() * 100);
     const result = (a + b).toString();
@@ -21,26 +23,33 @@ hearManager.hear('пример', async (context) => {
         `Решите пример: ${a} + ${b} = ?\n\nУ Вас есть 7 секунд.`, {
             answerTimeLimit: 7_000
         }
-    ).catch((err) => {
+    ).catch((error) => {
+        if (error instanceof TimeoutError) {
+            console.error('Превышено время ожидания ответа');
+            return null;
+        }
+
+        console.error(error);
         return null;
     });
 
-    // Значит ответ был дан позже лимита по времени
+    /**
+     * Пользователь не успел ответить
+     * или возникла другая ошибка
+     */
     if (answer === null) {
         return context.send('Вы не успели ответить :(');
     }
 
-    else if (answer.text && answer.text === result) {
-        return context.send('Вы решили пример верно!');
-    }
-
-    else if (answer.text && answer.text !== result) {
-        return context.send('Вы решили пример неверно!');
-    }
-
-    else if (!answer.text) {
+    if (!answer.text) {
         return context.send('Ответить нужно было текстом');
     }
+
+    const solved = answer.text === result;
+
+    return context.send(
+        `Вы решили пример ${!solved ? 'не' : ''}верно`
+    );
 });
 
 vk.updates.start().then(() => {
